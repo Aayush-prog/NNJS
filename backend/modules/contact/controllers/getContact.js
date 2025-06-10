56211a742e478495bf215b1af2f91d743f3714d0
Aayush <np03cs4a220238@heraldcollege.edu.np>
2025-06-10 11:50:45 +0545

    contact component

--- backend/modules/contact/controllers/getContact.js
+++ backend/modules/contact/controllers/getContact.js
@@ -3,10 +3,10 @@ const getContact = async (req, res) => {
   const COntactModel = mongoose.model("Contact");
   try {
     const contact = await COntactModel.find();
-    res.satus(201).json({
+    res.status(200).json({
       status: "success",
       message: "Contact found successfully",
-      data: contact,
+      data: contact[0],
     });
   } catch (error) {
     res.status(400).json({
