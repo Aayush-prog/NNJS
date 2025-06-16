7c74b1512452158338cc7d2dd8097e479b51c07e
Aayush <np03cs4a220238@heraldcollege.edu.np>
2025-06-16 13:10:23 +0545

    authcontext

---
+++ backend/models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    role: {
+      type: String,
+      default: ["admin", "staff"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    resetPasswordToken: {
+      type: String,
+      default: null,
+    },
+    resetPasswordExpires: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
